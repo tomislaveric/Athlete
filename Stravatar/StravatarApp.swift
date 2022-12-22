49fd537cb323c08e48d3aaba7c6f25c4debdb6ae
Tomislav Eric <hi@tomislav.mobi>
2022-12-22 11:20:17 +0100

    Initial Commit

---
+++ Stravatar/StravatarApp.swift
@@ -0,0 +1,17 @@
+//
+//  StravatarApp.swift
+//  Stravatar
+//
+//  Created by Tomislav Eric on 22.12.22.
+//
+
+import SwiftUI
+
+@main
+struct StravatarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
